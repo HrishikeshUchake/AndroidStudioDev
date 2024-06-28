7c79bbe46e261395848abcf39710ae69729f8755
Hrishikesh <uchakeh@gmail.com>
2024-06-28 16:45:42 +0530

    adding the view model experiment app folder

---
+++ CounterMVVM/app/src/main/java/com/example/countermvvm/CounterModel.kt
@@ -0,0 +1,14 @@
+package com.example.countermvvm
+
+data class CounterModel(var count: Int)
+
+class counterRepository{
+    private var _counter = CounterModel(0)
+    fun getCounter() = _counter
+    fun increment(){
+        _counter.count++
+    }
+    fun decrement(){
+        _counter.count--
+    }
+}
---
+++ CounterMVVM/app/src/main/java/com/example/countermvvm/CounterViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.countermvvm
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+    private val _counter = mutableStateOf(0)
+    val count: MutableState<Int> = _counter
+    fun increment(){
+        _counter.value++
+    }
+    fun decrement(){
+        _counter.value--
+    }
+}
